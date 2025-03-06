e3cf69bb111ae5b518b4ffb3af51764b972fe4d9
uchebuego <chuchuemenike@gmail.com>
2025-03-06 14:24:07 +0100

    (feature) create topic with given memo

--- features/step_definitions/create-simple-topic.ts
+++ features/step_definitions/create-simple-topic.ts
@@ -3,50 +3,84 @@ import {
   AccountBalanceQuery,
   AccountId,
   Client,
-  PrivateKey, RequestType,
-  TopicCreateTransaction, TopicInfoQuery,
-  TopicMessageQuery, TopicMessageSubmitTransaction
+  PrivateKey,
+  RequestType,
+  TopicCreateTransaction,
+  TopicInfoQuery,
+  TopicMessageQuery,
+  TopicMessageSubmitTransaction,
 } from "@hashgraph/sdk";
 import { accounts } from "../../src/config";
 import assert from "node:assert";
 import ConsensusSubmitMessage = RequestType.ConsensusSubmitMessage;
 
 // Pre-configured client for test network (testnet)
-const client = Client.forTestnet()
+const client = Client.forTestnet();
 
 //Set the operator with the account ID and private key
 
-Given(/^a first account with more than (\d+) hbars$/, async function (expectedBalance: number) {
-  const acc = accounts[0]
-  const account: AccountId = AccountId.fromString(acc.id);
-  this.account = account
-  const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
-  this.privKey = privKey
-  client.setOperator(this.account, privKey);
+Given(
+  /^a first account with more than (\d+) hbars$/,
+  async function (expectedBalance: number) {
+    const acc = accounts[0];
+    const account: AccountId = AccountId.fromString(acc.id);
+    this.account = account;
+    const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
+    this.privKey = privKey;
+    client.setOperator(this.account, privKey);
 
-//Create the query request
-  const query = new AccountBalanceQuery().setAccountId(account);
-  const balance = await query.execute(client)
-  assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance)
-});
+    //Create the query request
+    const query = new AccountBalanceQuery().setAccountId(account);
+    const balance = await query.execute(client);
+    assert.ok(balance.hbars.toBigNumber().toNumber() > expectedBalance);
+  }
+);
 
-When(/^A topic is created with the memo "([^"]*)" with the first account as the submit key$/, async function (memo: string) {
-});
+When(
+  /^A topic is created with the memo "([^"]*)" with the first account as the submit key$/,
+  async function (memo: string) {
+    const topicCreateTx = new TopicCreateTransaction()
+      .setTopicMemo(memo)
+      .freezeWith(client);
 
-When(/^The message "([^"]*)" is published to the topic$/, async function (message: string) {
-});
+    const topicCreateTxId = topicCreateTx.transactionId;
 
-Then(/^The message "([^"]*)" is received by the topic and can be printed to the console$/, async function (message: string) {
-});
+    console.log(
+      "The topic create transaction ID: ",
+      topicCreateTxId?.toString()
+    );
 
-Given(/^A second account with more than (\d+) hbars$/, async function () {
+    const topicCreateTxSigned = await topicCreateTx.sign(this.privKey);
 
-});
+    const topicCreateTxSubmitted = await topicCreateTxSigned.execute(client);
 
-Given(/^A (\d+) of (\d+) threshold key with the first and second account$/, async function () {
+    const topicCreateTxReceipt = await topicCreateTxSubmitted.getReceipt(
+      client
+    );
 
-});
+    const topicId = topicCreateTxReceipt.topicId;
+    console.log("topicId:", topicId?.toString());
+  }
+);
 
-When(/^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/, async function () {
+When(
+  /^The message "([^"]*)" is published to the topic$/,
+  async function (message: string) {}
+);
 
-});
+Then(
+  /^The message "([^"]*)" is received by the topic and can be printed to the console$/,
+  async function (message: string) {}
+);
+
+Given(/^A second account with more than (\d+) hbars$/, async function () {});
+
+Given(
+  /^A (\d+) of (\d+) threshold key with the first and second account$/,
+  async function () {}
+);
+
+When(
+  /^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/,
+  async function () {}
+);
