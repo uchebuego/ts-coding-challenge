e53f3f2db9b2eaddc60523a8e03f894be923b67d
uchebuego <chuchuemenike@gmail.com>
2025-03-07 13:08:29 +0100

    (done) create topic features

--- features/step_definitions/create-simple-topic.ts
+++ features/step_definitions/create-simple-topic.ts
@@ -11,25 +11,33 @@ import {
   TopicMessageQuery,
   TopicMessageSubmitTransaction,
   AccountCreateTransaction,
+  TopicId,
 } from "@hashgraph/sdk";
 import { accounts } from "../../src/config";
 import assert from "node:assert";
 import ConsensusSubmitMessage = RequestType.ConsensusSubmitMessage;
 
-// Pre-configured client for test network (testnet)
 const client = Client.forTestnet();
 
-//Set the operator with the account ID and private key
+const state = {
+  accountId: null as AccountId | null,
+  accountPrivateKey: null as PrivateKey | null,
+  secondAccountId: null as AccountId | null,
+  secondAccountPrivateKey: null as PrivateKey | null,
+  topicId: null as TopicId | null,
+  thresholdAccountId: null as AccountId | null,
+  thresholdKey: null as KeyList | null,
+};
 
 Given(
   /^a first account with more than (\d+) hbars$/,
   async function (expectedBalance: number) {
     const acc = accounts[0];
     const account: AccountId = AccountId.fromString(acc.id);
-    this.account = account;
+    state.accountId = account;
     const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
-    this.privKey = privKey;
-    client.setOperator(this.account, privKey);
+    state.accountPrivateKey = privKey;
+    client.setOperator(state.accountId, privKey);
 
     //Create the query request
     const query = new AccountBalanceQuery().setAccountId(account);
@@ -41,71 +49,63 @@ Given(
 When(
   /^A topic is created with the memo "([^"]*)" with the first account as the submit key$/,
   async function (memo: string) {
-    const topicCreateTx = new TopicCreateTransaction()
-      .setTopicMemo(memo)
-      .freezeWith(client);
+    if (!state.accountPrivateKey) {
+      throw new Error("Account private key is not set");
+    }
 
-    const topicCreateTxId = topicCreateTx.transactionId;
+    const topicCreateTx = new TopicCreateTransaction({
+      topicMemo: memo,
+    }).freezeWith(client);
 
-    console.log(
-      "The topic create transaction ID: ",
-      topicCreateTxId?.toString()
-    );
-
-    const topicCreateTxSigned = await topicCreateTx.sign(this.privKey);
-
-    const topicCreateTxSubmitted = await topicCreateTxSigned.execute(client);
+    const topicCreateTxSubmitted = await topicCreateTx.execute(client);
 
     const topicCreateTxReceipt = await topicCreateTxSubmitted.getReceipt(
       client
     );
 
-    this.topicId = topicCreateTxReceipt.topicId;
-    console.log("topicId:", this.topicId?.toString());
+    state.topicId = topicCreateTxReceipt.topicId;
+
+    if (!state.topicId) {
+      throw new Error("Failed to create topic: No topic ID returned");
+    }
+
+    console.log("Topic Created:", state.topicId.toString());
   }
 );
 
 When(
   /^The message "([^"]*)" is published to the topic$/,
   async function (message: string) {
-    const topicMsgSubmitTx = new TopicMessageSubmitTransaction()
-      .setTopicId(this.topicId)
-      .setMessage(message)
-      .freezeWith(client);
+    if (!state.topicId) {
+      throw new Error("No Topic ID found");
+    }
 
-    const topicMsgSubmitTxId = topicMsgSubmitTx.transactionId;
+    const topicMsgSubmitTx = new TopicMessageSubmitTransaction({
+      message,
+      topicId: state.topicId,
+    }).freezeWith(client);
 
-    console.log(
-      "The message submit create transaction ID: ",
-      topicMsgSubmitTxId?.toString()
-    );
+    const response = await topicMsgSubmitTx.execute(client);
+    const receipt = await response.getReceipt(client);
 
-    const topicMsgSubmitTxSigned = await topicMsgSubmitTx.sign(this.privKey);
-
-    const topicMsgSubmitTxSubmitted = await topicMsgSubmitTxSigned.execute(
-      client
-    );
-
-    const topicMsgSubmitTxReceipt = await topicMsgSubmitTxSubmitted.getReceipt(
-      client
-    );
-
-    const topicMsgSeqNum = topicMsgSubmitTxReceipt.topicSequenceNumber;
-    console.log("topicMsgSeqNum:", topicMsgSeqNum.toString());
+    console.log("Message published to topic: ", message);
   }
 );
 
 Then(
   /^The message "([^"]*)" is received by the topic and can be printed to the console$/,
   async function (message: string) {
+    if (!state.topicId) {
+      throw new Error("No Topic ID found");
+    }
+
     return new Promise((resolve, reject) => {
       const timeout = setTimeout(() => {
         subscription.unsubscribe();
         reject(new Error("Timeout waiting for message"));
       }, 4000);
-
       const subscription = new TopicMessageQuery()
-        .setTopicId(this.topicId)
+        .setTopicId(state.topicId!)
         .setStartTime(0)
         .subscribe(
           client,
@@ -116,7 +116,7 @@ Then(
           },
           (msg) => {
             const messageContent = Buffer.from(msg.contents).toString();
-            console.log("messageContent: ", messageContent);
+            console.log("Message received by Topic: ", messageContent);
 
             if (messageContent === message) {
               clearTimeout(timeout);
@@ -135,9 +135,11 @@ Given(
   async function (expectedBalance: number) {
     const acc = accounts[1];
     const account: AccountId = AccountId.fromString(acc.id);
-    this.account = account;
+
+    state.secondAccountId = account;
+
     const privKey: PrivateKey = PrivateKey.fromStringED25519(acc.privateKey);
-    this.privKey2 = privKey;
+    state.secondAccountPrivateKey = privKey;
 
     const query = new AccountBalanceQuery().setAccountId(account);
     const balance = await query.execute(client);
@@ -149,46 +151,70 @@ Given(
 Given(
   /^A (\d+) of (\d+) threshold key with the first and second account$/,
   async function (threshold: number, total: number) {
-    this.thresholdKey = new KeyList(
-      [this.privKey.publicKey, this.privKey2.publicKey],
+    if (!state.accountPrivateKey || !state.secondAccountPrivateKey) {
+      throw new Error("Account private keys are not set");
+    }
+
+    state.thresholdKey = new KeyList(
+      [
+        state.accountPrivateKey.publicKey,
+        state.secondAccountPrivateKey.publicKey,
+      ],
       threshold
     );
 
-    const transaction = new AccountCreateTransaction()
-      .setKey(this.thresholdKey)
-      .setInitialBalance(1000);
+    const transaction = new AccountCreateTransaction({
+      key: state.thresholdKey,
+      initialBalance: 10,
+    }).freezeWith(client);
 
     const response = await transaction.execute(client);
     const receipt = await response.getReceipt(client);
-    const newAccountId = receipt.accountId;
 
-    console.log("New Account ID with Threshold Key:", newAccountId?.toString());
+    state.thresholdAccountId = receipt.accountId;
+
+    if (!state.thresholdAccountId) {
+      throw new Error(
+        "Failed to create threshold account: No account ID returned"
+      );
+    }
+
+    console.log("Threshold Account ID:", state.thresholdAccountId.toString());
   }
 );
 
 When(
   /^A topic is created with the memo "([^"]*)" with the threshold key as the submit key$/,
   async function (memo: string) {
-    const topicCreateTx = new TopicCreateTransaction()
-      .setTopicMemo(memo)
+    if (!state.accountPrivateKey || !state.secondAccountPrivateKey) {
+      throw new Error("Account private keys are not set");
+    }
+
+    const topicCreateTx = new TopicCreateTransaction({ topicMemo: memo })
+      .setNodeAccountIds([new AccountId(3)])
       .freezeWith(client);
 
-    const topicCreateTxId = topicCreateTx.transactionId;
+    const sig1 = state.accountPrivateKey.signTransaction(topicCreateTx);
+    const sig2 = state.secondAccountPrivateKey.signTransaction(topicCreateTx);
 
-    console.log(
-      "Threshold Key topic create transaction ID: ",
-      topicCreateTxId?.toString()
-    );
+    const signedTransaction = topicCreateTx
+      .addSignature(state.accountPrivateKey.publicKey, sig1)
+      .addSignature(state.secondAccountPrivateKey.publicKey, sig2);
 
-    const topicCreateTxSigned = await topicCreateTx.sign(this.thresholdKey);
-
-    const topicCreateTxSubmitted = await topicCreateTxSigned.execute(client);
-
+    const topicCreateTxSubmitted = await signedTransaction.execute(client);
     const topicCreateTxReceipt = await topicCreateTxSubmitted.getReceipt(
       client
     );
 
-    this.topicId = topicCreateTxReceipt.topicId;
-    console.log("topicId:", this.topicId?.toString());
+    if (!topicCreateTxReceipt.topicId) {
+      throw new Error(
+        "Failed to create topic with threshold key: No topic ID returned"
+      );
+    }
+
+    console.log(
+      "Threshold Key used to create Topic:",
+      topicCreateTxReceipt.topicId.toString()
+    );
   }
 );
